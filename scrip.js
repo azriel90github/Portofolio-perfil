45990307636adb07e8fc509a5719c2d8466791f2
azriel90github <azrielgithub@gmail.com>
2024-01-23 13:02:08 +0000

    05

--- scrip.js
+++ scrip.js
@@ -1,16 +1,33 @@
+//Relógio Digital
+const hour = document.querySelector('#hour')
+const min = document.querySelector('#min')
+const sec = document.querySelector('#sec')
+
+setInterval(() => {
+  let date = new Date()
+  let dHour = date.getHours()
+  let dMinute = date.getMinutes()
+  let dSec = date.getSeconds()
+
+  hour.innerHTML = `${formatTime(dHour)}`
+  min.innerHTML = `${formatTime(dMinute)}`
+  sec.innerHTML = `${formatTime(dSec)}`
+
+}, 1000)
+
+function formatTime(time) {
+  return time < 10 ? '0' + time : time
+}
 
 
-
-/*
-let relogioum = document.querySelector('.relogio')
-let relogio = relogioum.newDate
-
-
-let hora = relogio.getHours()
-
-
-relogioum.innerHTML=`${hora}`
-*/
+function myMenuFunction() {
+  var i = document.getElementById("navMenu");
+  if(i.className === "nav-menu") {
+      i.className += " responsive";
+  } else {
+      i.className = "nav-menu";
+  }
+ }
 
 
 
