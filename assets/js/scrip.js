ca35b2a11691ad64783c914a10e8b09253f62bc6
Azriel Armando <114200974+azriel90github@users.noreply.github.com>
2024-04-10 21:44:20 +0100

    '10'

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -21,6 +21,19 @@ function formatTime(time) {
 }
 
 
+$(document).ready(function () {
+  $('#close').click(function () {
+    $('#mobile').fadeOut('fast', function () {});
+  });
+  $('#open').click(function () {
+    $('#mobile').fadeIn('fast', function () {});
+  });
+  $('#about').click(function () {
+    $('#tab').slideToggle('normal', function () {});
+  });
+});
+
+
 
 // Eventos typelt para a digitação automática
 document.addEventListener('DOMContentLoaded', () => {
