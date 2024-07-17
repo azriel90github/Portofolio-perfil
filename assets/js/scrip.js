cd5d31438f293eb486baf0990ecd4e17db5bf17c
Azriel Armando <114200974+azriel90github@users.noreply.github.com>
2024-07-17 12:46:18 +0100

    novo portfolio

---
+++ assets/js/scrip.js
@@ -0,0 +1,31 @@
+
+// Programa para o Relógio Digital
+const hour = document.querySelector('#hour')
+const min = document.querySelector('#min')
+const sec = document.querySelector('#sec')
+
+setInterval(() => {
+  let date = new Date()
+  let dHour = date.getHours()
+  let dMinute = date.getMinutes()
+  let dSec = date.getSeconds()
+
+  hour.innerHTML = `${formatTime(dHour)}`
+  min.innerHTML = `${formatTime(dMinute)}`
+  sec.innerHTML = `${formatTime(dSec)}`
+
+}, 1000)
+
+function formatTime(time) {
+  return time < 10 ? '0' + time : time
+}
+
+
+
+
+
+
+
+
+
+
