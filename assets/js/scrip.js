946450793d23b193990df50726426da139971c36
Azriel Armando <114200974+azriel90github@users.noreply.github.com>
2024-07-19 22:10:26 +0100

    menu

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -20,6 +20,14 @@ function formatTime(time) {
   return time < 10 ? '0' + time : time
 }
 
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
 
 
 
