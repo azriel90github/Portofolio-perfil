116e7d8a0bccd432a3cdf746f2de20da83328915
Azriel Armando <114200974+azriel90github@users.noreply.github.com>
2024-07-24 17:21:12 +0100

    update script

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -62,44 +62,34 @@ navbar.classList.remove('active');
 
 };
 
-
 new TypeIt("#hero", {
-    speed: 50,
-    startDelay: 900,
+  speed: 50,
+  startDelay: 900,
+})
+  .type("I'm the Suéli Armando", { delay: 100 })
+  .move(-8, { delay: 100 })
+  .type("o", { delay: 400 })
+  .move(null, { to: "START", instant: true, delay: 300 })
+  .move(1, { delay: 200 })
+  .delete(1)
+  .type(", ", { delay: 225 })
+  .pause(200)
+  .move(2, { instant: true })
+  .pause(200)
+  .move(5, { instant: true })
+  .move(5, { delay: 200 })
+  .type("a", { delay: 350 })
+  .move(null, { to: "END" })
+  .type("le typing utlity")
+  .move(-4, { delay: 150 })
+  .type("i")
+  .move(null, { to: "END" })
+  .type(' Devoloper <span class="place">Software</span>', { delay: 400 })
+  .delete(".place", { delay: 800, instant: true })
+  .type('<em><strong class="font-semibold"> Extreme.</strong></em>', {
+    speed: 100,
   })
-    .type("Sou o Suélio rmando", { delay: 100 })
-    .move(-8, { delay: 100 })
-    .type("e", { delay: 400 })
-    .move(null, { to: "START", instant: true, delay: 300 })
-    .move(1, { delay: 200 })
-    .delete(1)
-    .type("l", { delay: 225 })
-    .pause(200)
-    .move(2, { instant: true })
-    .pause(200)
-    .move(5, { instant: true })
-    .move(5, { delay: 200 })
-    .type("A", { delay: 350 })
-    .move(null, { to: "END" })
-    .type("le typing utlity")
-    .move(-4, { delay: 150 })
-    .type("i")
-    .move(null, { to: "END" })
-    .type(' Desenvolvedor <span class="place">internet</span>', { delay: 400 })
-    .delete(".place", { delay: 800, instant: true })
-    .type('<em><strong class="font-semibold">planet.</strong></em>', {
-      speed: 100,
-    })
-    .go();
-
-
-
-
-
-
-
-
-
+  .go();
 
 
 
