64d2bd24ceadb688bdaca23b8b399f9bb791253a
Azriel Armando <114200974+azriel90github@users.noreply.github.com>
2024-08-05 21:37:09 +0100

    color p segundo update

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -81,7 +81,7 @@ new TypeIt("#hero", {
   .type(" and")
   .move(null, { to: "END" })
   .type(' Devoloper<span class="place"> Software</span>', { delay: 400 })
-  .type('<em><strong class="font-semibold"> Extreme</strong> JavaScript.</em>', {
+  .type('<em><strong class="font-semibold"> Extreme</strong> JavaScript and Go.</em>', {
     speed: 100,
   })
   .go();
